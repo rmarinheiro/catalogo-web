a8c89d3066853caf9eaa1b9a3dcea3162728b5fd
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2022-03-18 11:43:51 -0300

    Navbar hamburger

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,35 @@
 import React from "react";
 import "assets/styles/custom.scss";
 import "App.css";
-import './styles.css'
+import "./styles.css";
+import 'bootstrap/js/src/collapse.js';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md bg-primary main-nav">
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
       <div className="container-fluid">
         <a href="link" className="nav-logo">
           <h4>Catalogo Web</h4>
         </a>
-        <div className="collapse navbar-collapse">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#catalog-navbar"
+          aria-controls="catalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="catalog-navbar">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link" className="active">HOME</a>
+              <a href="link" className="active">
+                HOME
+              </a>
             </li>
             <li>
               <a href="link">CATÁLOGO</a>
