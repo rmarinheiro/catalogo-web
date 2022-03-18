0a25e119a2886d5b36043b6b4215e192c2d87e47
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2022-03-18 12:08:54 -0300

    Add Images

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./assets/styles/custom.scss";
 import "./App.css";
 import Navbar from "components/Navbar";
+import Home from "pages/Home";
 
 function App() {
-  return <Navbar />;
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "assets/styles/custom.scss";
+import "App.css";
+import Navbar from "components/Navbar";
+import { ReactComponent as Image } from "assets/images/main-home.svg";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <h1>Conheça Nosso catalogo de Produtos</h1>
+            <div className="home-image-container">
+                <Image/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
