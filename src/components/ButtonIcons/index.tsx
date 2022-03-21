4b988392c773a872c79abf6153cdf7e37b7dbcdf
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2022-03-21 19:51:04 -0300

    Tornando a pagina Home responsiva

---
+++ src/components/ButtonIcons/index.tsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+const ButtonIcon = () =>{
+    return(
+        <button className="btn btn-primary btn-icon">
+            <h6>Inicie Sua Busca</h6>
+        </button>
+    );
+
+}
+
+export default ButtonIcon;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,8 @@ import "assets/styles/custom.scss";
 import "App.css";
 import Navbar from "components/Navbar";
 import { ReactComponent as Image } from "assets/images/main-home.svg";
-import './styles.css';
+import "./styles.css";
+import ButtonIcon from "components/ButtonIcons";
 
 const Home = () => {
   return (
@@ -12,10 +13,12 @@ const Home = () => {
       <div className="home-container">
         <div className="home-card">
           <div className="home-content-container">
-            <h1>Conheça Nosso catalogo de Produtos</h1>
-            <div className="home-image-container">
-                <Image/>
-            </div>
+            <h1>Conheça Nosso Catálogo de Produtos</h1>
+            <p>Ajudaremos você a encontrar os melhores produtos</p>
+            <ButtonIcon/>
+          </div>
+          <div className="home-image-container">
+            <Image />
           </div>
         </div>
       </div>
